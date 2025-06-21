79c2dbfef793a369d382fc8bd7d840ea93c91b15
Saidi <saidi@step.ai>
2025-06-21 17:38:30 +0800

    add ios code

--- ios/ReactNativePcmPlayerModule.swift
+++ ios/ReactNativePcmPlayerModule.swift
@@ -1,48 +1,28 @@
 import ExpoModulesCore
 
 public class ReactNativePcmPlayerModule: Module {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
-  public func definition() -> ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ReactNativePcmPlayer')` in JavaScript.
+  public override func definition() -> ModuleDefinition {
     Name("ReactNativePcmPlayer")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants([
-      "PI": Double.pi
-    ])
+    Events("onMessage", "onStatus")
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      return "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { (value: String) in
-      // Send an event to JavaScript.
-      self.sendEvent("onChange", [
-        "value": value
-      ])
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of the
-    // view definition: Prop, Events.
-    View(ReactNativePcmPlayerView.self) {
-      // Defines a setter for the `url` prop.
-      Prop("url") { (view: ReactNativePcmPlayerView, url: URL) in
-        if view.webView.url != url {
-          view.webView.load(URLRequest(url: url))
+    Function("enqueuePcm") { (base64Data: String) in
+      if let data = Data(base64Encoded: base64Data) {
+        CurrentAudioPlayer.shared.enqueuePcmData(data) { status in
+          self.sendEvent("onStatus", [
+            "status": status
+          ])
         }
       }
+    }
 
-      Events("onLoad")
+    Function("stopCurrentPcm") {
+      CurrentAudioPlayer.shared.stopAndRelease()
+    }
+
+    Function("markAsEnded") {
+      CurrentAudioPlayer.shared.markAsEnded()
     }
   }
 }
+
